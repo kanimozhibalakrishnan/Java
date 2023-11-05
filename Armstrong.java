139061e976066b548a2c905d4fc31148fe1a513d
Kanimozhi BV <125288031+kanimozhibalakrishnan@users.noreply.github.com>
2023-11-05 12:02:00 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,17 +1,23 @@
-import java.*;
+import java.lang.*;
+import java.util.*;
+
+
 class Armstrong{
 	public static void main(String[] argv){
-		int n1,n2,rem,org=0,res;
+		int n1,n2,rem,org=0,res=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number:");
+		n1=sc.nextInt();
 		n2=n1;
-		while(n1>0)
+		while(n2>0)
 		{
-			rem=n1%10;
-			res+=n1*3;
-			n1=n1/10;
+			rem=n2%10;
+			res+=Math.pow(rem,3);
+			n2=n2/10;
 		}
-		if(res==n2)
-			system.out.println("Armstrong num is:",res);
+		if(res==n1)
+			System.out.println(n1+" is a Armstrong num");
 		else
-			system.out.println(" not an Armstrong num is:",res);
+			System.out.println(n1+" is not an Armstrong num");
 	}
 }
