c6266e8777166390ac8138890e81fe43a34d6d45
Kanimozhi BV <125288031+kanimozhibalakrishnan@users.noreply.github.com>
2023-11-19 21:48:47 +0530

    Add files via upload

---
+++ printstmt.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+class printstmt{
+	public static void main(String[] args){
+		System.out.println("Enter any number:");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		System.out.println("The number is "+num);
+	}
+}
