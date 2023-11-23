9c91c00b4428f097ec70f34a3c009c6acfb15862
Kanimozhi BV <125288031+kanimozhibalakrishnan@users.noreply.github.com>
2023-11-23 21:45:30 +0530

    Add files via upload

---
+++ Randominterger.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+class Randominteger{
+	public static void main(String[] args){
+		System.out.println("Random number1:"+ Math.random());
+		System.out.println("Random number2:"+ Math.random());
+		System.out.println("Random number3:"+ Math.random());
+		System.out.println("Random number4:"+ Math.random());
+	}
+}
