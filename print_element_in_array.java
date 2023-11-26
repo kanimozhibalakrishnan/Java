2b4a049520fea5e24edfba3ed6851f15c496b021
Kanimozhi BV <125288031+kanimozhibalakrishnan@users.noreply.github.com>
2023-11-26 10:57:39 +0530

    Add files via upload

---
+++ print_element_in_array.java
@@ -0,0 +1,11 @@
+public class print_element_in_array {
+    public static void main(String[] args) {
+        int arr[]=new int[]{1,2,3,4,5,6,7,8,9,0};
+
+        //printing the element of the array
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+}
